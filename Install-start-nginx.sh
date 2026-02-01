6f8682a020346474b4bfc1b6dd69e7d861a8d984
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-01 22:59:54 +0530

    modified-2

--- Install-start-nginx.sh
+++ Install-start-nginx.sh
@@ -4,12 +4,12 @@ echo -e "I am installing and starting nginx via shell scripting"
 
 nginx -v
 
-if [$? ne 0] then
+if [$? ne 0]; then
    sudo dnf install nginx -y 
    sudo systemctl start nginx
    sudo systemctl enable nginx
-else
-   echo -e "nginx is already installed 
+   else
+      echo -e "nginx is already installed 
 fi
 
 
