a98a9503870fed5b2bbfed8d7b32bf6197318a7f
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-01 17:49:20 +0530

    Install-nginx-t1

---
+++ Install-start-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "I am installing and starting nginx via shell scripting"
+
+nginx -v
+
+if ($? ne 0) then
+   sudo dnf -f install nginx -y 
+   sudo systemctl start nginx
+   sudo systemctl enable nginx
+else
+   echo -e "nginx is already installed 
+fi
+
+
+
