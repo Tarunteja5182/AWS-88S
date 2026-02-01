4ade98ff8693dde4867cb503746ff03856351491
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-01 23:43:02 +0530

    logs-commit-1

--- Install-start-nginx.sh
+++ Install-start-nginx.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-user_id = $(id -u)
+user_id=$(id -u)
 logs_folder="/var/log/shellscript"
 logs_file="/var/log/shellscript/$0.log"
 
+mkdir -p "$logs_folder"
+
+
 if [ $user_id -ne 0 ]; then
-   echo "Please run the script with sudo user" | tee -a $LOGS_FILE
+   echo "Please run the script with sudo user" | tee -a $logs_file
    exit 1
 fi
  
-echo -e "I am installing and starting nginx via shell scripting"
+echo "I am installing and starting nginx via shell scripting"
 
-nginx -v &|tee -a $logs_file
+nginx -v |tee -a $logs_file
 
 if [ $? -ne 0 ]; then
    sudo dnf install nginx -y &>> $logs_file
    sudo systemctl start nginx
    sudo systemctl enable nginx
 else
-      echo -e "nginx is already installed" | tee -a $LOGS_FILE
+      echo -e "nginx is already installed" | tee -a $logs_file
 fi
 
 
