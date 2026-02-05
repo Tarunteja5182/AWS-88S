fc9bc122390f5e839714972845e2a62683a079c0
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-05 21:45:41 +0530

    word-count-final

--- word-count.sh
+++ word-count.sh
@@ -3,9 +3,9 @@
 # This will count no of words in a given text file
 
 file2read=$1
-ls -lrth|grep $file2read
+ls -lrth|grep $file2read > /dev/null 2>&1
 if [ $? -eq 2 ];then
-  echo "no such file or file or directory $file2read"
+  echo "no such file or directory $file2read"
   exit 1
 fi
 
